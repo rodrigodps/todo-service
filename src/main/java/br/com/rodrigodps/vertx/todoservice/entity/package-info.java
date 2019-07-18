eeb18386a9b28e9cddcf7f728f20ec33dbe520d1
Rodrigo de Paula <rodrigodps@gmail.com>
2019-07-18 09:28:07 -0300

    Project start

---
+++ src/main/java/br/com/rodrigodps/vertx/todoservice/entity/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Indicates that this module contains classes that need to be generated / processed.
+ */
+@ModuleGen(name = "vertx-todo-service-entity", groupPackage = "br.com.rodrigodps.vertx.todoservice.entity")
+package br.com.rodrigodps.vertx.todoservice.entity;
+
+import io.vertx.codegen.annotations.ModuleGen;
