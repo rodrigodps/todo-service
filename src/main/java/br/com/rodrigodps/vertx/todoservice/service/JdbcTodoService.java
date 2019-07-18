8738987b4cbdad2d5d4509ae9ea88d1d19326397
Rodrigo de Paula <rodrigodps@gmail.com>
2019-07-18 14:41:17 -0300

    Initializing the JDBC Client

--- src/main/java/br/com/rodrigodps/vertx/todoservice/service/JdbcTodoService.java
+++ src/main/java/br/com/rodrigodps/vertx/todoservice/service/JdbcTodoService.java
@@ -4,13 +4,45 @@ import br.com.rodrigodps.vertx.todoservice.entity.Todo;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 public class JdbcTodoService implements TodoService {
 
+    private static final Logger LOGGER = Logger.getLogger(JdbcTodoService.class.getName());
+
+    private static final String SQL_CREATE = "CREATE TABLE IF NOT EXISTS `todo` (\n" +
+            "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+            "  `title` varchar(255) DEFAULT NULL,\n" +
+            "  `completed` tinyint(1) DEFAULT NULL,\n" +
+            "  `order` int(11) DEFAULT NULL,\n" +
+            "  `url` varchar(255) DEFAULT NULL,\n" +
+            "  PRIMARY KEY (`id`) )";
+    private static final String SQL_INSERT = "INSERT INTO `todo` " +
+            "(`id`, `title`, `completed`, `order`, `url`) VALUES (?, ?, ?, ?, ?)";
+    private static final String SQL_QUERY = "SELECT * FROM todo WHERE id = ?";
+    private static final String SQL_QUERY_ALL = "SELECT * FROM todo";
+    private static final String SQL_UPDATE = "UPDATE `todo`\n" +
+            "SET `id` = ?,\n" +
+            "`title` = ?,\n" +
+            "`completed` = ?,\n" +
+            "`order` = ?,\n" +
+            "`url` = ?\n" +
+            "WHERE `id` = ?;";
+    private static final String SQL_DELETE = "DELETE FROM `todo` WHERE `id` = ?";
+    private static final String SQL_DELETE_ALL = "DELETE FROM `todo`";
+
+    private final Vertx vertx;
+    private final JsonObject config;
+    private final JDBCClient client;
+
     public JdbcTodoService(Vertx vertx, JsonObject config) {
+        this.vertx = vertx;
+        this.config = config;
+        this.client = JDBCClient.createShared(vertx, config);
     }
 
     @Override
